95d4dc3a6bfd83f254eb3cb8f101310b1f94dc6b
ilyj <i.lyj@me.com>
2014-01-07 10:14:02 +0800

    js综合案例

---
+++ myschool/js/mytest.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+var i;
+document.writeln("测试！");
+for(i=0;i<5;i++)
+{
+	document.writeln("测试" + i);
+}
+var str = prompt("ttt","tttttt");
+document.writeln(str);
+document.writeln(typeof(12.12));
+document.writeln(parseInt('86'));
+function showPr(){
+	var j;
+	for(j = 0;j < 10;j++){
+		document.writeln("helloworld");
+	}
+}
+
